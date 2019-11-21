85b44f4eec0669613e99c16daaf12a6f37887bc7
Kelly Selden <kellyselden@gmail.com>
2019-11-21 12:33:37 +0000

    ember-cli 3.13

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module'
@@ -16,6 +17,8 @@ module.exports = {
     browser: true
   },
   rules: {
+    'ember/no-jquery': 'error',
+
     // https://github.com/ember-cli/eslint-plugin-ember/issues/604
     'ember/no-invalid-debug-function-arguments': 'off'
   },
